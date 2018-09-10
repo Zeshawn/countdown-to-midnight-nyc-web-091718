f16c0ecd801bce589506988b1dc2d9f49e6992bf
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-10 22:55:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
-  puts "#{seconds_to_midnight} SECOND(S)!"
-  seconds-=1
-end
-"HAPPY NEW YEAR!"
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
